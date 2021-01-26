0db47b3318b3e1d799a60f88e2bdcba7e3d54c83
Jose Rodriguez <boriel@gmail.com>
2021-01-26 22:39:10 +0100

    Add first paragraph
    This commit should have JIRA-1234: ticket mark in the title

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ If you print mymodule.__doc__ its value is this comment.
 
 When no docstring is specified, __doc__ equals to None
 """
+
+HELP = """
+REGULAR EXPRESSIONS 
+
+Introduction
+============
+
+1. Regular expressions (called REs, or regexes, or regex patterns) are essentially
+a tiny, highly specialized programming language embedded inside Python and made
+available through the :mod:`re` module. Using this little language, you specify
+the rules for the set of possible strings that you want to match; this set might
+contain English sentences, or e-mail addresses, or TeX commands, or anything you
+like.  You can then ask questions such as "Does this string match the pattern?",
+or "Is there a match for the pattern anywhere in this string?".  You can also
+use REs to modify a string or to split it apart in various ways.
+"""
