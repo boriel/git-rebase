c45dc8709888708b3db1215aba3d0f28be5c81ac
Jose Rodriguez <boriel@gmail.com>
2021-01-26 22:29:21 +0100

    First import

---
+++ main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+""" The first comment in a module is the module docstring.
+If you print mymodule.__doc__ its value is this comment.
+
+When no docstring is specified, __doc__ equals to None
+"""
